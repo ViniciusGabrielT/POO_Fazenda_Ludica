2b315630777f26f8759782cfcde7fb55c37c3e9b
Vinicius Torres <viniciusgabriellopestorres@outlook.com>
2025-10-18 13:36:26 -0300

    Inserindo encapsulamento e criando objetos

--- fazenda_ludica.py
+++ fazenda_ludica.py
@@ -2,8 +2,8 @@
 
 class Animal:
     def __init__(self, nome, idade):
-        self._nome = nome
-        self._idade = idade
+        self.nome = nome
+        self.idade = idade
 
     def emitir_som(self):
         return "O animal emite um som."
@@ -32,7 +32,19 @@ class Gato(Animal):
 class Vaca(Animal):
     def __init__(self, producao_leite_litros):
         super().__init__()
-        self.producao_leite_litros = producao_leite_litros
+        self.__producao_leite_litros = producao_leite_litros
 
     def emitir_som(self):
         return "Muuu!"
+    
+    def obter_producao_leite(self): # getter
+        return self.__producao_leite_litros
+    
+    def registrar_ordenha(self, litros): # setter
+        self.__producao_leite_litros = litros
+
+# Teste e Demonstração
+
+cachorro = Cachorro("Rex", 3, "Labrador")
+gato = Gato("Mimi", 5, "Branco")
+vaca = Vaca("Mimosa", 7, 25.5)
