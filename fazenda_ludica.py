d8f73f963afea0e8bd0475722a836c1e167210b1
Vinicius Torres <viniciusgabriellopestorres@outlook.com>
2025-10-18 12:55:23 -0300

    Inserindo as classes filhas Gato e Vaca

--- fazenda_ludica.py
+++ fazenda_ludica.py
@@ -2,8 +2,8 @@
 
 class Animal:
     def __init__(self, nome, idade):
-        self.__nome = nome
-        self.__idade = idade
+        self._nome = nome
+        self._idade = idade
 
     def emitir_som(self):
         return "O animal emite um som."
@@ -20,3 +20,19 @@ class Cachorro(Animal):
 
     def emitir_som(self):
         return "Au! Au!"
+    
+class Gato(Animal):
+    def __init__(self, cor_pelo):
+        super().__init__()
+        self.cor_pelo = cor_pelo
+
+    def emitir_som(self):
+        return "Miau!"
+    
+class Vaca(Animal):
+    def __init__(self, producao_leite_litros):
+        super().__init__()
+        self.producao_leite_litros = producao_leite_litros
+
+    def emitir_som(self):
+        return "Muuu!"
